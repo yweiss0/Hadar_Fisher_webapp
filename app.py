d0c14d3f95f40ca3dbd9553dccc7d3f0392395c2
yweiss0 <yweiss0@gmail.com>
2025-02-13 11:28:40 -0500

    added the 3d birds affect to the main page

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 
 import streamlit as st
+import streamlit.components.v1 as components
+
+
 
 st.set_page_config(page_title="ML Analysis Dashboard", layout="wide")
+components.html(
+    open("threed.html").read(),
+    height=800, # Full screen height width=None,  # Full width
+    width=None,  # Full width
+    scrolling=False
+)
+# st.title("📊 Can Natural Language Processing Track Negative Emotions in the Daily Lives of Adolescents?")
 
-st.title("📊 Machine Learning Model Analysis")
-
-st.write("Welcome to the Machine Learning Model Analysis Dashboard. Navigate using the sidebar to explore model performance and predictions.")
+# st.write("Welcome to the Machine Learning Model Analysis Dashboard. Navigate using the sidebar to explore model performance and predictions.")
 
 st.sidebar.success("Select a page above.")
+
+
+
+
+
+# Embed Three.js visualization
