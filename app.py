265d27d9002fef31d8f5ec2e82eb639c13e9212e
yweiss0 <yweiss0@gmail.com>
2025-02-25 08:55:16 -0500

    added page icon

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 
 
-st.set_page_config(page_title="ML Analysis Dashboard", layout="wide")
+st.set_page_config(page_title="ML Analysis Dashboard", page_icon="📊", layout="wide")
 components.html(
     open("threed.html").read(),
     height=800, # Full screen height width=None,  # Full width
